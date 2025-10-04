d1b25c713c023b9940dfe834c7204986ca094ff2
KelvinCodesInC <kelvino28@nycstudents.net>
2025-10-04 11:45:56 -0400

    Update Level.java

--- src/Level.java
+++ src/Level.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+public class Level
+{
+    private boolean goalReached;
+    private int points;
 
-public class Level {
+    /** Returns true if the player reached the goal on this level and returns false otherwise */
+    public boolean goalReached()
+    {
+        return goalReached;
+    }
+
+    public void setGoalReached(boolean isGoalReached) {
+        goalReached = isGoalReached;
+    }
+    /** Returns the number of points (a positive integer) recorded for this level */
+    public int getPoints()
+    {
+        return points;
+    }
+
+    public void setPoints(int p) {
+        this.points = p;
+    }
+// There may be instance variables, constructors, and methods that are not shown.
 }
